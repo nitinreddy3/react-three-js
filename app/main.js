98ee191085371f3c99f0bf0a02d1fdf39ed6c226
Nitin Reddy <nitin.reddy@weboniselab.com>
2017-07-26 17:55:36 +0530

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,6 @@
+require('./theme/default/stylesheets/main.sass');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Simple from './javascript/components/Simple';
+
+ReactDOM.render(<Simple/>, document.getElementById('app'));
